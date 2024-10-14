8adcc1c1c1d15c58b067aa155532f214523756f8
Alex Darr <alexdarr@gmail.com>
2024-10-14 15:31:07 -0400

    Add ActiveStorage

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_17_150546) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_14_193023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "billing_months", force: :cascade do |t|
     t.bigint "business_unit_id", null: false
@@ -30,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_17_150546) do
     t.datetime "updated_at", null: false
     t.text "repos", default: [], array: true
     t.string "prefix"
+    t.integer "unit_type", default: 0, null: false
   end
 
   create_table "polling_statuses", force: :cascade do |t|
@@ -59,6 +88,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_17_150546) do
     t.index ["org", "name"], name: "index_repos_on_org_and_name", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "billing_months", "business_units"
   add_foreign_key "repo_costs", "repos"
   add_foreign_key "repos", "business_units"
